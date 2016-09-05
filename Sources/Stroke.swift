a74c4c4fb3dcb586b38f1b6c85cc2da9fe87070a
Christian Paul Dehli <cpdehli@gmail.com>
2016-09-05 07:21:08 -0400

    Trying to get archiving working.

--- Sources/Stroke.swift
+++ Sources/Stroke.swift
@@ -7,19 +7,37 @@
 
 
 /// a drawing stroke
-public class Stroke {
+public class Stroke: NSObject, NSCoding {
+    
     /// the points that make up the stroke
     internal var points: [String]!
+    
     /// the properties of the stroke
     internal var settings: StrokeSettings!
     
-    init() {
+    override init() {
+        super.init()
         self.points = [];
         self.settings = StrokeSettings()
     }
     
     init(points: [String], settings: StrokeSettings) {
+        super.init()
         self.points = points
         self.settings = settings
-    }    
+    }
+    
+    // MARK: NSCoding
+    
+    required public convenience init?(coder aDecoder: NSCoder) {
+        self.init()
+        
+        self.points = aDecoder.decodeObjectForKey("points") as! [String]!
+        self.settings = aDecoder.decodeObjectForKey("settings") as! StrokeSettings!
+    }
+    
+    public func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeObject(self.points, forKey: "points")
+        aCoder.encodeObject(self.settings, forKey: "settings")
+    }
 }
